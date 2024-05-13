fc3269ba309687ae396dad2e6a3515e2565f37f3
OLUWAFUNMILAYO <166605083+Anuoluwa59@users.noreply.github.com>
2024-05-13 14:07:11 +0100

    fix issue with user login

--- user/common.py
+++ user/common.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from models import User, Data 
+from models import User, Data
+from flask_login import current_user
 from utils.common import train_model
 
 def load_and_process_db_data(user_data):
@@ -31,8 +32,8 @@ def get_trained_model(data):
     return train_model(data)
 
 def get_logged_in_user(db_session):
-    # user = current_user
-    return db_session.query(User).get(2)
+    user = current_user
+    return db_session.query(User).get(int(user.id))
 
 def get_and_load_user_data(db_session):
     user = get_logged_in_user(db_session)
